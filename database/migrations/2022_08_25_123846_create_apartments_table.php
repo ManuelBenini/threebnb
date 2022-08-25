ead7bd3a2b9c5e00430e3eade45b1b905e118d21
ManuelBenini <manuelbenini1905@gmail.com>
2022-08-25 16:25:51 +0200

    Popolato il database creando migration/model/seeder

---
+++ database/migrations/2022_08_25_123846_create_apartments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->unsignedTinyInteger('rooms');
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('bathrooms');
+            $table->unsignedSmallInteger('sqm');
+            $table->string('address');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->string('image');
+            $table->string('image_original_name');
+            $table->boolean('visible')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
---
+++ database/seeds/ServiceSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Service;
+use Illuminate\Database\Seeder;
+
+class ServiceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $services = ['Wifi', 'Posto macchina', 'Piscina', 'Portineria', 'Sauna', 'Vista mare', 'Aria condizionata', 'BBQ'];
+
+        foreach ($services as $service) {
+            $new_service = new Service;
+            $new_service->name = $service;
+            $new_service->save();
+        }
+    }
+}
