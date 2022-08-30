8713962a871d80caebab61ee96a6a1e44061337c
ManuelBenini <manuelbenini1905@gmail.com>
2022-08-30 20:59:03 +0200

    Rimosse delle variabili dichiarate in-line. Rese un po' più piccole le immagini generate dal Faker .

--- database/seeds/ApartmentSeeder.php
+++ database/seeds/ApartmentSeeder.php
@@ -13,7 +13,7 @@ class ApartmentSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i=0; $i < 20; $i++) { 
+        for ($i=0; $i < 20; $i++) {
             $new_apartment = new Apartment();
             $new_apartment->title = $faker->title;
             $new_apartment->rooms = $faker->numberBetween(2, 10);
@@ -21,9 +21,9 @@ class ApartmentSeeder extends Seeder
             $new_apartment->bathrooms = $faker->numberBetween(1, 10);
             $new_apartment->sqm = $faker->numberBetween(10, 150);
             $new_apartment->address = $faker->address();
-            $new_apartment->latitude = $faker->latitude($min = -90, $max = 90);
-            $new_apartment->longitude = $faker->longitude($min = -180, $max = 180);
-            $new_apartment->image = $faker->imageUrl(640, 480, 'houses', true);
+            $new_apartment->latitude = $faker->latitude(-90, 90);
+            $new_apartment->longitude = $faker->longitude(-180, 180);
+            $new_apartment->image = $faker->imageUrl(400, 400, 'houses', true);
             $new_apartment->image_original_name = $faker->city();
             $new_apartment->visible = $faker->boolean();
             $new_apartment->save();
