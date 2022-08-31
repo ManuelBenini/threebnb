b077dc3b729810d272d79ae25a602d77ba660d53
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-08-31 15:32:32 +0200

    finito parte sponsorship

--- resources/js/components/pages/ApartmentPage.vue
+++ resources/js/components/pages/ApartmentPage.vue
@@ -2,18 +2,18 @@
     <div class="container">
        
         <ApartmentDetails/>
-         <MapComp/>
-        
+        <MapComp/>
     </div>
 </template>
 
 <script>
 import MapComp from '../elements/MapComp.vue';
 import ApartmentDetails from '../sections/ApartmentDetails.vue';
+import SponsorshipsComp from '../sections/SponsorshipsComp.vue';
 
     export default {
     name: "ApartmentPage",
-    components: { MapComp, ApartmentDetails}
+    components: { MapComp, ApartmentDetails, SponsorshipsComp }
 }
 </script>
 
--- resources/js/components/sections/SponsorshipsComp.vue
+++ resources/js/components/sections/SponsorshipsComp.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div class="sponsor">
 
     <div class="container text-center m-5">
       <h1>Sponsorizza il tuo annuncio</h1>
@@ -9,17 +9,45 @@
       
         <div class="d-flex card text-center m-2 lite">
             <h1>Lite</h1><span class="d-block">2,99 &euro;</span>
-            <div class="mt-4"><p>Ottieni un posizionamento di rilievo per <strong>24h</strong></p></div>
+            <div class="mt-4"><p>Ottieni un posizionamento di rilievo per</p></div>
+            <h1>24h</h1>
+            <a href="#scegli" @click="checkLite" class="seleziona">Seleziona</a>
         </div>
         <div class="d-flex card text-center m-2 plus">
+            <p class="popolare position-absolute price-badge font-weight-normal">Più popolare</p>
             <h1>Plus</h1><span class="d-block sbarra">8,97 &euro;</span><span class="d-block">5,99 &euro;</span>
-            <div class="mt-4"><p>Ottieni un posizionamento di rilievo per <strong>72h</strong></p></div>
+            <div class="mt-4"><p>Ottieni un posizionamento di rilievo per</p></div>
+            <h1>72h</h1>
+            <a href="#scegli" @click="checkPlus" class="seleziona">Seleziona</a>
         </div>
        
         <div class="d-flex card text-center m-2 gold">
             <h1>Gold</h1><span class="d-block sbarra">17,94 &euro;</span><span class="d-block">9,99 &euro;</span>
-            <div class="mt-4"><p>Ottieni un posizionamento di rilievo per <strong>144h</strong></p></div>
+            <div class="mt-4"><p>Ottieni un posizionamento di rilievo per</p></div>
+            <h1>144h</h1>
+            <a href="#scegli" @click="checkGold" class="seleziona">Seleziona</a>
         </div>
+    </div>
+
+    <div id="scegli" class="d-flex align-items-center justify-content-center flex-column">
+      <div>
+        <h3>Seleziona il tipo di sponsorizzazione</h3>
+      </div>
+      
+      <div class="d-flex flex-row">
+        <p>
+        Lite <input type="radio" name="sponsor" value="lite" :checked="liteChecked" />
+        </p>
+        <p>
+        Plus <input type="radio" name="sponsor" value="plus" :checked="plusChecked" />
+        </p>
+        <p>
+        Gold <input type="radio" name="sponsor" value="gold" :checked="goldChecked" />
+      </p>
+      </div>
+      
+    
+      
     </div>
 
   </div>
@@ -27,14 +55,38 @@
 
 <script>
     export default {
-        name: 'SponsorshipsComp'
+        name: 'SponsorshipsComp',
+        data(){
+          return {
+            liteChecked: false,
+            plusChecked: false,
+            goldChecked: false,
+          }
+        },
+        methods:{
+          checkLite(){
+            this.liteChecked = true
+          },
+          checkPlus(){
+            this.plusChecked = true
+          }, 
+          checkGold(){
+            this.goldChecked = true
+          }  
+        }
+
     }
+    
 </script>
 
 <style lang="scss" scoped>
 
 body{
     background:#eee;
+}
+
+.sponsor{
+  margin-bottom: 100px;
 }
 
 #scelto{
@@ -48,7 +100,7 @@ body{
   padding: 10px;
   border-radius: 20px;
   color: #fff;
-  height: 350px;
+  height: 400px;
   display: flex;
   position: relative;
   align-items: center;
@@ -59,11 +111,13 @@ body{
 .lite{
   color: #0e0e0e;
   background-color: #ffffff;
-  border: solid 3px black;
+  border: solid 3px #EB594E;
 }
 
 .lite:hover, .plus:hover, .gold:hover{
-  border: solid 3px #EB594E;
+  background-color: #EB594E;
+  background-image: none;
+  color: white;
 }
 
 .plus{
@@ -71,14 +125,14 @@ body{
   background-image: url(https://media.istockphoto.com/photos/abstract-metal-background-silver-gray-background-picture-id1287620067?b=1&k=20&m=1287620067&s=170667a&w=0&h=fZF45_nnF2owHwG11jRUcm8ibFirkUo1kZ1aozGJvCs=);
   background-position: top;
   background-size: cover;
-  border: solid 3px black;
+  border: solid 3px #EB594E;
 }
 
 .gold{
   color: #404040;
   background-image: url(https://i.pinimg.com/originals/1e/c8/d2/1ec8d279aacf9eae231a6b9945b8bdd3.jpg);
   background-position: center;
-  border: solid 3px black;
+  border: solid 3px #EB594E;
 }
 
 
@@ -108,5 +162,52 @@ body{
 .sbarra{
   text-decoration: line-through;
 }
+
+.popolare{
+  bottom: -63px;
+  background-color: #EB594E;
+  color: #fefefe;
+  font-weight: 600;
+  font-size: 24px;
+  border: solid 2px #EB594E;
+  padding: 3px;
+  border-radius: 0px 0px 20px 20px;
+}
+
+.seleziona{
+  padding: 10px;
+  color: White;
+  background-color: #212529;
+  margin-top: 15px;
+  border-radius: 25px;
+}
+
+#scegli{
+  margin-top: 100px;
+  height: 300px;
+  width: 100%;
+  & a{
+  color: white;
+  background-color: #EB594E;
+  font-size: 20px;
+  font-weight: 600;
+  padding: 10px;
+  margin: 30px;
+  border-radius: 20px;
+  }
+  & a:hover{
+  background-color: #404040;
+  }
+  & p{
+  margin: 22px;
+  font-size: 22px;
+  font-weight: 600;
+  }
+}
+
+#buttonpay{
+  margin: 20px;
+}
+
 
 </style>
