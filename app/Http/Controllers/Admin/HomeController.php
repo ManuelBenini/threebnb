5ad2f7a7eee98dc01f1135dc8a632c95a114602c
ManuelBenini <manuelbenini1905@gmail.com>
2022-09-03 05:38:28 +0200

    Creata funzione che restituisce solo gli appartamenti dell'utente loggato ed inseriti nella dashboard.

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Apartment;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('admin.home');
+        $userId = Auth::id();
+        $userApartments = Apartment::where('user_id', '=', $userId)->paginate(3);
+        return view('admin.home', compact('userApartments'));
     }
 }
