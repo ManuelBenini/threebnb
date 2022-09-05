435109160d91e50c298f962b238fb800c7781d4e
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-09-05 18:13:26 +0200

    inserimento titolo

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,45 +3,30 @@
 @section('content')
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-
 
     <!-- Nuova sezione Dashboard -->
-    <div>
-
+   
     
     <div class="dashboard container-fluid">
 
-        <div class="row mt-5 d-flex justify-content-center">
+        <div class="row mb-5 d-flex justify-content-center">
             <div class="user col-lg-2 col-sm-12">
                 <div class="col text-center mt-3 profile">
                     <ul>
                         <li><img src="https://upload.wikimedia.org/wikipedia/commons/e/ee/Unknown-person.gif" alt=""></li>
                         <li><h4>{{ Auth::user()->name }}</h4></li>
-                        <li><a href="#">MODIFICA PROFILO</a></li>
+                        <li><h4>{{ Auth::user()->surname }}</h4></li>
                     </ul>
                 </div>
 
             </div>
 
 
+
             <div class="col-9 ">
+                <div>
+                    <h1 class="titolo">I MIEI APPARTAMENTI</h1>
+                </div>
                 @foreach ($userApartments as $apartment)
                 <div class="colonnamiei col-l-4 col-sm-6 col-12 app text-center py-2" v-for="(apartment,  index) in apartmentsList" :key="`apartment${index}`">
 
@@ -55,7 +40,7 @@
                             <a class="visualizza bottinte" href="{{route('admin.apartments.show', $apartment)}}">Visualizza</a>
                         </li>
                         <li>
-                            <a class="statistiche bottinte" href="">Statistiche</a>
+                            <a class="statistiche bottinte" href="">Sponsorizza</a>
                         </li>
                         <li>
                             <a class="modifica bottinte" href="{{route('admin.apartments.edit', $apartment)}}">Modifica</a>
@@ -65,18 +50,22 @@
                 </div>
                 @endforeach
 
-                <div class="col-12 d-flex justify-content-center my-3">
-                    
-                </div>
+                
             </div>
 
+            <div class="col-12 d-flex justify-content-center my-3">
+                <h1>I TUOI MESSAGGI</h1>
+            </div>
 
+            <div>
+                
+            </div>
 
         </div>
 
     </div>
 
-    </div>
+    
 
 </div>
 @endsection
