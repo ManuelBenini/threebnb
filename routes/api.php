6fe4d9b4949b743db1481b90f9e8934f9c0d0dfc
ManuelBenini <manuelbenini1905@gmail.com>
2022-09-07 02:46:51 +0200

    Fix chiamata API con TUTTI i filtri!

--- routes/api.php
+++ routes/api.php
@@ -22,8 +22,6 @@ Route::namespace('Api')
     Route::get('/sponsored', 'PageController@getSponsoredApartments');
     Route::get('/sponsoredPaginate', 'PageController@getSponsoredApartmentsPaginate');
     Route::get('/services', 'PageController@getServices');
-    Route::get('/filteredApartments/{rooms}/{beds}/{distance}/{lat}/{lon}', 'PageController@apartmentsWithFilters');
-    Route::get('/filteredSponsored/{rooms}/{beds}/{distance}/{lat}/{lon}', 'PageController@sponsoredWithFilters');
     Route::get('/apartment-details/?id={id}', 'PageController@show');
-    // Route::get('/prova-1/{services}', 'PageController@apWithServices');
+    Route::get('/filteredApartments/{rooms}/{beds}/{distance}/{lat}/{lon}/{servicesList}/{sponsored}', 'PageController@apartmentsWithFilters');
 });
